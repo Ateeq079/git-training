60efb94a014cbf2b6dd1ab3bb8025012cfa53f01
Ateeq079 <rateequr079@gmail.com>
2025-03-15 21:41:07 +0500

    Commit_1

---
+++ ne.py
@@ -0,0 +1,12 @@
+# demo_script.py
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Git practice."
+
+def main():
+    name = input("Enter your roll number: ")
+    print(greet(name))
+
+if __name__ == "__main__":
+    main()
+
---
+++ prctc.py
@@ -0,0 +1,12 @@
+# demo_script.py
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Git practice."
+
+def main():
+    name = input("Enter your name: ")
+    print(greet(name))
+
+if __name__ == "__main__":
+    main()
+
